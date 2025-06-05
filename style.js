727b8f47c8588d4588f5d7ecabb68f1ff28f71d6
PinePlayz <amir2548896@gmail.com>
2025-06-04 20:17:21 -0400

    added half working mobile support

---
+++ style.js
@@ -0,0 +1,9 @@
+function ismobile() {
+    return /iPhone|iPad|iPod|Mobi|Android/i.test(navigator.userAgent);
+};
+if (ismobile()) {
+    //console.log("It's mobile matey john or something");
+    //const style = document.getElementById('style');
+    //style.setAttribute('href', 'mobile.css');
+    window.location.replace("/mobileindex.html");
+}
